ea69c6fea3870be2fc82370266c1214f5197fb86
Ralcy <ralcyv@gmail.com>
2017-10-31 15:03:48 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # execute code
-python ./src/di_challenge_ralcy.py ./input/itcont.txt ./output/medianvals_by_zip.txt ./output/medianvals_by_date.txt
+python ./src/di_challenge_ralcy.py
